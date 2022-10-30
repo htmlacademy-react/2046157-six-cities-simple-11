a12835d712fa7d2fc04472bda4b6a80556d34bfd
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-10-30 21:39:46 +0200

    added privateRoute component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
-import { Navigate, BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import PlaceScreen from '../../pages/place-screen/place-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import Layout from '../layout/layout';
+import PrivateRoute from '../private-route/private-route';
 
 import { AppRoute } from '../../types/paths';
 import { Place, User } from '../../types/data';
@@ -21,7 +22,12 @@ function App(props: AppProps): JSX.Element {
       <Routes>
         <Route path={AppRoute.Root} element={<Layout user={props.user} />}>
           <Route index element={<MainScreen placesCount={props.placesCount} places={props.places} />} />
-          <Route path={AppRoute.Login} element={props.user.id ? <Navigate to={AppRoute.Root} /> : <LoginScreen />} />
+          <Route path={AppRoute.Login} element={
+            <PrivateRoute user={props.user}>
+              <LoginScreen />
+            </PrivateRoute>
+          }
+          />
           <Route path={`${AppRoute.Place}/:id`} element={<PlaceScreen places={props.places} user={props.user} />} />
         </Route>
         <Route path='*' element={<NotFoundScreen />} />
